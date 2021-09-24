f5abf90ba4da59b0c3b5ea741df669bdc38e1bb1
Mr Dark <84636949+Ravindhu123@users.noreply.github.com>
2021-09-24 18:01:49 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *Dark-Bot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *Dark-Bot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *Dark-Bot*:  ```" + msg + "```"
 }
 
 
